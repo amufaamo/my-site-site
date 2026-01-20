6f5cf27671e99051f20e1087e913339f84782c46
Masakazu Ishikawa/石川昌和/いしかわまさかず <45814539+amufaamo@users.noreply.github.com>
2026-01-20 16:42:15 +0900

    Add Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://USERNAME.github.io',
+  site: 'https://amufaamo.github.io',
   base: '/my-site-site',
   output: 'static',
   build: {
